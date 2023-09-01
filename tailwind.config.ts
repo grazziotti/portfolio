37584e4112ee82d35c926e6f0b5c4bcec37b39ad
Daniel <dgplays14@gmail.com>
2023-09-01 13:26:36 -0300

    style: added color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,18 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  darkMode: ['class', "data-theme='dark'"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        target: 'rgb(var(--color-target) / <alpha-value>)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
