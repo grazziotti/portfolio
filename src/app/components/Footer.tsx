ef63a17e83da68a440696366bf39cce5abbf7a4e
Daniel <dgplays14@gmail.com>
2023-09-11 01:14:49 -0300

    feat: add footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="mx-auto w-full max-w-5xl border-t py-8 text-center text-lg">
+        © 2023 / Daniel Grazziotti
+      </div>
+    </footer>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { About } from './components/About'
 import { Contact } from './components/Contact'
+import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
 import { Projects } from './components/Projects'
@@ -16,7 +17,7 @@ export default function Home() {
         <Skills />
         <Contact />
       </main>
-      <footer>footer</footer>
+      <Footer />
     </>
   )
 }
