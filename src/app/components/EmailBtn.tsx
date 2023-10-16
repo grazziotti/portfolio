6c03e1c53ff7033d65d549d2c5d8e9194c30487d
Daniel <dgplays14@gmail.com>
2023-10-16 04:19:48 -0300

    feat: update contact email

--- src/app/components/EmailBtn.tsx
+++ src/app/components/EmailBtn.tsx
@@ -6,7 +6,7 @@ import copy from 'copy-to-clipboard'
 import { Mail } from 'lucide-react'
 
 export const EmailBtn = () => {
-  const email = 'grazziotti.contato@gmail.com'
+  const email = 'danielgrazziotti99@gmail.com'
   const [copied, setCopied] = useState(false)
 
   const handleCopyEmail = () => {
