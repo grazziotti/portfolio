6645dc680e8253d8cff8d4cff608ebf3657516c9
Daniel <dgplays14@gmail.com>
2023-10-22 09:41:18 -0300

    feat: add google verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,11 @@ const saira = Saira({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Portfólio | Daniel Grazziotti',
-  description: 'Portfólio de Daniel Grazziotti'
+  description: 'Portfólio de Daniel Grazziotti',
+  verification: {
+    google: 'NK_YRyzH-6vt7WN_fUlh27kl9Rbv60jNBhDY_H7oLb8'
+  }
 }
-
 export default function RootLayout({
   children
 }: {
